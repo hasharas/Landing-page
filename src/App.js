012714fb5a84e853aa734c61eb5528ed39d051f9
hasharas <hasharamsankalpam@gmail.com>
2024-04-06 10:40:55 +0530

    develop questions space

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import CompanyVision from './components/CompanyVision';
 import CompanyWorlflow from './components/CompanyWorlflow';
+import FAQ from './components/FAQ';
+
 function App() {
   return (
     <div className="App">
         <Header />
         <CompanyVision />
         <CompanyWorlflow />
+        <FAQ />
     </div>
   );
 }
---
+++ src/components/FAQ.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa";
+
+const FAQ = () => {
+  return (
+    <div className='mx-auto'>
+        <h2 className='text-[27px] text-[#6B3CC9] poppins-semibold mt-24 mb-10'>Frequently asked questions</h2>
+        <FAQCard title={"Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?"} text={"Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer."} />
+        <FAQCard title={"Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?"} text={"Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer."} />
+        <FAQCard title={"Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?"} text={"Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer."} />
+    </div>
+  )
+}
+
+const FAQCard = ({title, text}) => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleActive = () => {
+        setIsActive(!isActive);
+    }
+
+    return (
+        <div className={`bg-[#FAF8FF] ${isActive?"text-[#6B3CC9] h-[150px] ":"text-black h-[80px]"}  transition-all duration-300 lg:w-[896px] w-full p-[24px] mx-auto flex flex-col gap-[13px]`}>
+            <div className='flex justify-between items-center cursor-pointer' onClick={handleActive}>
+                <h3 className="text-left">{title}</h3>
+                {isActive?<FaMinus />:<FaPlus />}
+            </div>
+            {isActive?<p className='text-[#6F6C90] text-justify fadeIn'>{text}</p>:<></>}
+        </div>
+    )
+}
+
+export default FAQ
