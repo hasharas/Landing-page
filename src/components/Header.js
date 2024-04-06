a7a5324221b705a45b061ada150258ed0ca7aa27
hasharas <hasharamsankalpam@gmail.com>
2024-04-06 09:11:06 +0530

    add topnavigation and header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import TopNavigation, { HamburgerMenu } from './TopNavigation'
+import { IoMenu } from "react-icons/io5";
+import LOGO from '../asset/logo.svg'
+
+const Header = () => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(()=>{
+        const handleWindowWidth = () => {
+            setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener("resize",handleWindowWidth)
+    });
+
+  return (
+    <div className='w-full h-[77px] flex justify-between items-center px-[80px] py-[26px] bg-[#6B3CC9]'>
+        <img src={LOGO} alt='logo' />
+        {windowWidth>768?<TopNavigation />:
+            <div>
+                <IoMenu className='text-white text-4xl' onClick={() => setIsActive(true)} />
+                <HamburgerMenu isActive={isActive} setIsActive={setIsActive} />   
+            </div>}
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/TopNavigation.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { ImCross } from "react-icons/im";
+
+const TopNavigation = () => {
+  return (
+    <div className='w-[381px] h-[17px] text-white flex justify-between items-center' >
+      <a href='#'>SERVICES</a>
+      <a href='#'>ABOUT US</a>
+      <a href='#'>CONTACT US</a>
+      <a href='#'>CAREERS</a>
+    </div>
+  )
+}
+
+export const HamburgerMenu = ({isActive, setIsActive}) => {
+  return (
+    <div className={`fixed px-[20px] py-[16px] top-0 ${isActive?"right-0":"right-[-100%]"}  transition-all duration-300 flex flex-col gap-[36px] items-start h-[812px] w-[375px] bg-white`}>
+      <div className='flex w-full justify-between items-start'>
+        <a href='#'>HOME</a>
+        <ImCross onClick={() => setIsActive(false)} />
+      </div>
+      <a href='#'>SERVICES</a>
+      <a href='#'>ABOUT US</a>
+      <a href='#'>CONTACT US</a>
+      <a href='#'>CAREERS</a>
+    </div>
+  )
+}
+
+export default TopNavigation
